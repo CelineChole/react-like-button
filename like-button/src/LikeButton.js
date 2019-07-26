e437cb6d75a5252e4df64b8ab27d0947f7294855
celinechole <github@celinechole.com>
2019-07-26 17:55:53 -0400

    add logo and favicon

---
+++ like-button/src/LikeButton.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class LikeButton extends Component {
+    render() {
+        return (
+        <div>
+            HEY FROM LIKE BUTTON
+        </div>)
+    }
+}
+
+export default LikeButton;
