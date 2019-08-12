a0a153c048bd8d71b98c7856a7a1827fe54f7684
celinechole <github@celinechole.com>
2019-08-12 15:15:01 -0400

    update like button class

--- like-button/src/LikeButtonClass.js
+++ like-button/src/LikeButtonClass.js
@@ -25,17 +25,17 @@ class LikeButtonClass extends Component {
     }
 
     setCount = (e) => {
-      // setState is an async function. 
-      // React may batch a bunch of setStates together. 
-      // So the value of this.state.count is the value at the time you make the request.
-      this.setState((prevState, props) => ({
-        count: prevState.count + 1
-    })); 
+      // setState is an async function. React may batch a bunch of setStates together. 
+      // To be sure the value of this.state.count is the value at the time you make the request, we can use the below code:
+      // this.setState((prevState, props) => ({
+      //   count: prevState.count + 1
+      // })); 
 
+      // In our example it is acceptable to use the below code:
       this.setState({
-        count: this.state.count + 1 
+        count: this.state.count + 1
       });
-      localStorage.setItem(LIKE_KEY, newCount)
+      localStorage.setItem(LIKE_KEY, this.state.count)
     }
 
 
